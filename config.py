284bf7e79ac5df2e5ad579c16cc828fda12ddbc1
Michael Freitas <michael_rfreitas@hotmail.com>
2022-05-26 23:17:27 +0000

    code: validate pwd and create a config file

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 """ This module to create a config script importing devices module """
-from devices import ConfSwitchL3
+
 import os
 import re
+import shutil
+import datetime
+from devices import ConfSwitchL3
+from pathlib import Path
 
 
 class ConfQuestionsSWL3(ConfSwitchL3):
@@ -11,39 +15,109 @@ class ConfQuestionsSWL3(ConfSwitchL3):
 
         return f'''
         ---
-        {self.hostname}
-        {self.username}
+        Hostname: {self.hostname}
+        Username: {self.username}
+        Password: {self.password}
+        Manager VLAN ID: {self.manager_vlan}
+        Manager IP address: {self.manager_ip}
+        Manager Subnet Mask: {self.manager_mask}
+        Primary Interface VLAN ID: {self.primary_int_vlan}
+        Primary Interface IP address: {self.primary_int_ip}
+        Primary Interface Subnet Mask: {self.primary_int_mask}
+        Default Gateway: {self.default_gateway}
         ---
         '''
 
     def return_full_config(self):
-        pass
+        date = datetime.datetime.now()
+
+        base_path_temp = Path('assets/templates/c_sw_l3.txt')
+        base_path_conf = Path('assets/configs/' +
+                              self.hostname + '_' + date.strftime('%d') + '-' + date.strftime('%b') + '-' + date.strftime('%Y') + '.txt')
+
+        shutil.copyfile(base_path_temp, base_path_conf)
+
+        reading_file = open(base_path_temp, "r")
+        writing_file = open(base_path_conf, 'w')
+
+        find_conf = ("${hostname}", "${username}", "${password}",
+                     "${manager_vlan}", "${manager_ip}", "${manager_mask}",
+                     "${primary_int_vlan}", "${primary_int_ip}",
+                     "${primary_int_mask}", "${default_gateway}")
+        repl_conf = (self.hostname, self.username, self.password,
+                     self.manager_vlan, self.manager_ip,
+                     self.manager_mask, self.primary_int_vlan,
+                     self.primary_int_ip, self.primary_int_mask,
+                     self.default_gateway)
+
+        for line in reading_file:
+            for find, repl in zip(find_conf, repl_conf):
+                line = line.replace(find, repl)
+            writing_file.write(line)
+        reading_file.close()
+        writing_file.close()
 
     def questions(self):
-        self.hostname = input('Whats the hostname? ')
-        self.username = input('Whats the username? ')
-        self.password = input('Whats the password? ')
         while True:
-            self.manager_vlan = input('Whats the manager VLAN ID? (1-4094): ')
+            self.hostname = input('What is the hostname? (SW-HOST-01): ')
+            if validate_text(self.hostname):
+                clear_terminal()
+                break
+        while True:
+            self.username = input('What is the username? (local-admin): ')
+            if validate_text(self.username):
+                clear_terminal()
+                break
+        while True:
+            self.password = input(
+                'What is the password? (Minimun 8 characteres): ')
+            if validate_pwd(self.password):
+                clear_terminal()
+                break
+        while True:
+            self.manager_vlan = input(
+                'What is the manager VLAN ID? (1-4094): ')
             if validate_vlan(self.manager_vlan):
                 clear_terminal()
                 break
         while True:
             self.manager_ip = input(
-                'Whats the manager IP? (XXX.XXX.XXX.XXX): ')
+                'What is the manager IP address? (XXX.XXX.XXX.XXX): ')
             if validate_ip(self.manager_ip):
                 clear_terminal()
                 break
-        self.manager_mask = input(
-            'Whats the manager mask? (XXX.XXX.XXX.XXX): ')
-        self.primary_int_vlan = input(
-            'Whats the primary interface VLAN? (1-4094): ')
-        self.primary_int_ip = input(
-            'Whats the primary interface IP? (XXX.XXX.XXX.XXX): ')
-        self.primary_int_mask = input(
-            'Whats the primary interface mask? (XXX.XXX.XXX.XXX): ')
-        self.default_gateway = input(
-            'Whats the default gateway? (XXX.XXX.XXX.XXX): ')
+        while True:
+            self.manager_mask = input(
+                'What is the manager subnet mask? (XXX.XXX.XXX.XXX): ')
+            if validate_ip(self.manager_mask):
+                clear_terminal()
+                break
+        while True:
+            self.primary_int_vlan = input(
+                'What is the primary interface VLAN ID? (1-4094): ')
+            if validate_vlan(self.primary_int_vlan):
+                clear_terminal()
+                break
+        while True:
+            self.primary_int_ip = input(
+                'What is the primary interface IP address? ' +
+                '(XXX.XXX.XXX.XXX): ')
+            if validate_ip(self.primary_int_ip):
+                clear_terminal()
+                break
+        while True:
+            self.primary_int_mask = input(
+                'What is the primary interface subnet mask? ' +
+                '(XXX.XXX.XXX.XXX): ')
+            if validate_ip(self.primary_int_mask):
+                clear_terminal()
+                break
+        while True:
+            self.default_gateway = input(
+                'What is the default gateway? (XXX.XXX.XXX.XXX): ')
+            if validate_ip(self.default_gateway):
+                clear_terminal()
+                break
 
 
 def validate_vlan(answer):
@@ -87,7 +161,7 @@ def validate_ip(answer):
     regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
 
     try:
-        if not (re.search(regex, answer)):
+        if not re.search(regex, answer):
             clear_terminal()
             raise ValueError(
                 f'''
@@ -113,6 +187,25 @@ def validate_text(answer):
                 This text {answer} is incorrect,
                 please, be sure to type it correctly
                 minimum 5 and maximum 20 characters
+                --------------- WARNING ---------------
+                '''
+            )
+    except ValueError as err:
+        clear_terminal()
+        print(f"{err}")
+        return False
+    return True
+
+
+def validate_pwd(answer):
+    try:
+        if len(answer) < 8:
+            raise ValueError(
+                f'''
+                --------------- WARNING ---------------
+                This password {answer} is incorrect,
+                please, be sure to type it correctly
+                minimum 8 characters
                 --------------- WARNING ---------------
                 '''
             )
